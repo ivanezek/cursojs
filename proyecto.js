503916145d8c3b0575400fda71ed3444e4aa6591
ivanezek <ivanrosaleset24@gmail.com>
2022-04-21 02:25:35 -0300

    Eziv Gs 1.0

--- proyecto.js
+++ proyecto.js
@@ -1,14 +1,16 @@
 // PRODUCTOS
 const listaProductos = [
-    {id: 1,name: "FIFA 22", price: 7500, img: 'assets/fifa.jpg'},
+    {id: 1, name: "Assassins Creed: Origins", price: 6900, img: 'assets/ac_origins.jpg'},
     {id: 2, name: "Resident Evil: Biohazard", price: 2800, img: 'assets/re7.jpg'},
     {id: 3, name: "Red Dead Redemption", price: 1500, img: 'assets/rdr.jpg'},
     {id: 4, name: "Batman: Arkham Knight", price: 1200, img: 'assets/batman_arkham.jpg'},
-    {id: 5, name: "Assassins Creed: Origins", price: 6900, img: 'assets/ac_origins.jpg'},
+    {id: 5,name: "FIFA 22", price: 7500, img: 'assets/fifa.jpg'},
     {id: 6, name: "The Last Of Us: Part 2", price: 1300, img: 'assets/tlou_2.png'},
     {id: 7, name: "Cyberpunk 2077", price: 6000, img: 'assets/cyberp.jpg'},
     {id: 8, name: "NBA 2k19", price: 900, img: 'assets/nba.jpg'}
 ]
+
+// GENERACIÓN DE CATALOGO CON DOM.
 
 const catalogo = document.getElementById('productos')
 for(const producto of listaProductos) {
@@ -22,11 +24,28 @@ for(const producto of listaProductos) {
         </div>
         <div class="item-price">
             <p class='precios'> $ ${producto.price}</p>
-        </div> 
+        </div>
+        <div class="item-button">
+            <button class='btn btn-dark botonCarrito' id='idJuego-${producto.id}'>Comprar</button>   
+        </div>
+
     `
     catalogo.appendChild(div)
 }
 
+// ELECCIÓN DE PRODUCTO con EVENTO.
+
+const botonesCarrito = document.getElementsByClassName('botonCarrito')
+for (const boton of botonesCarrito) {
+    boton.onclick = addCarrito
+}
+
+function addCarrito(e) {
+    const boton = e.target
+    const id = boton.id.split('-')[1]
+    const juego = listaProductos.find(p => p.id == id)
+    console.log('Agregando producto ', juego)
+}
 
 //herramientas
 const suma = (a, b) => a + b
